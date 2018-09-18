7fa3973ee1b51e06728cb5fdb8e79cab2df828a9
wule61 <wule61@live.com>
2018-09-19 00:19:37 +0800

    ?

--- zookeeper/main.go
+++ zookeeper/main.go
@@ -7,23 +7,64 @@ import (
 	"github.com/samuel/go-zookeeper/zk"
 )
 
+var hosts = []string{"localhost:2181"}
+var path = "/watch"
+var flags int32 = zk.FlagEphemeral
+var data1 = []byte("hello,this is a zk go")
+var acls = zk.WorldACL(zk.PermAll)
+
 func main() {
-	conn, err := getConnect()
+	option := zk.WithEventCallback(callback)
+	conn, _, err := zk.Connect(hosts, time.Second*5, option)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		return
 	}
 	defer conn.Close()
-	conn.Create("/go_servers", nil, 0, zk.WorldACL(zk.PermAll))
+	create(conn, path, data1)
 
-	time.Sleep(20 * time.Second)
+	_, _, ech, err := conn.ExistsW(path)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+START:
+	for {
+		select {
+		case _, ok := <-ech:
+			if ok {
+				break START
+			} else {
+				fmt.Println("节点不存在")
+			}
+
+		}
+	}
+
 }
 
-func getConnect() (conn *zk.Conn, err error) {
-	var hosts = []string{"localhost:2181"} //server端host
-	conn, _, err = zk.Connect(hosts, time.Second*5)
-	if err != nil {
-		fmt.Println("错误:", err)
-		return nil, err
+func watchCreataNode(ech <-chan zk.Event) {
+	event := <-ech
+	fmt.Println("********channel***********")
+	fmt.Println("path:", event.Path)
+	fmt.Println("type:", event.Type.String())
+	fmt.Println("state:", event.State.String())
+	fmt.Println("-------------------")
+}
+
+func callback(event zk.Event) {
+	fmt.Println("********callback***********")
+	fmt.Println("path:", event.Path)
+	fmt.Println("type:", event.Type.String())
+	fmt.Println("state:", event.State.String())
+	fmt.Println("-------------------")
+}
+
+//创建节点
+func create(conn *zk.Conn, path string, data []byte) {
+	_, err_create := conn.Create(path, data, flags, acls)
+	if err_create != nil {
+		fmt.Println(err_create)
+		return
 	}
-	return conn, err
 }
